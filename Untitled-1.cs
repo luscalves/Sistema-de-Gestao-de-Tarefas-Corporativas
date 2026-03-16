214bcfc447f7226b61be4817be37fcd700e71e14
luscalves <lucalves14@hotmail.com>
2026-03-16 11:50:01 -0300

    Segund commit

--- Untitled-1.cs
+++ Untitled-1.cs
@@ -1,7 +1,56 @@
 namespace SistemaTarefasCorporativas.Entities
 {
-    public class Entidade
+    public class Tarefa
     {
-        // TODO: Adicione aqui as propriedades, construtores e métodos da sua entidade
+        public int Id{get; private set;}
+        public int? UsuarioAtribuidoId{get; private set;}
+        public string Titulo{get; private set;}
+        public string Descricao{get; private set;}
+        public StatusTarefa Status{get; private set;}
+        public DateTime DataCriacao{get; private set;}
+        public string MotivoBloqueio{get; private set;}
+
+        public enum StatusTarefa
+        {
+            Pendente,
+            EmAndamento,
+            Concluida,
+            Bloqueada
+        }
+
+        public Tarefa(string titulo, string descricao)
+        {
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Status = StatusTarefa.Pendente;
+            this.DataCriacao = DateTime.UtcNow;
+        }
+
+        public void Concluir(){
+           if(this.UsuarioAtribuidoId == null){
+            throw new Exception("Ação inválida: Tarefa concluída sem usuário atribuído.");
+           } else{
+            this.Status = StatusTarefa.Concluida;
+           }
+        }
+
+        public void Bloquear(string MotivoBloqueio){
+            if(string.IsNullOrEmpty(MotivoBloqueio)){
+                throw new Exception("Ação inválida: tarefa bloqueada sem motivo atribuído.");
+            } else {
+                this.Status = StatusTarefa.Bloqueada;
+                this.MotivoBloqueio = MotivoBloqueio;
+            }
+        }
+
+        public void AtribuirUsuario(int usuarioId){
+            if(this.UsuarioAtribuidoId != null){
+                throw new Exception("Ação inválida: tarefa já atribuida para um usuário");
+            } else{
+                this.UsuarioAtribuidoId = usuarioId;
+                this.Status = StatusTarefa.EmAndamento;
+            }
+        }
+
     }
 }
