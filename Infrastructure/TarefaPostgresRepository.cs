62cff896d49be7eb55cd5bb064ae5dc3bc8632c3
Lucas Alves <114171968+luscalves@users.noreply.github.com>
2026-05-11 09:51:02 -0300

    Clean up comments in TarefaPostgresRepository

--- Infrastructure/TarefaPostgresRepository.cs
+++ Infrastructure/TarefaPostgresRepository.cs
@@ -32,16 +32,13 @@ namespace SistemaDeGestaoDeTarefas.Infrastructure
 
         public void Remover(Tarefa tarefa)
         {
-            // O Entity Framework apenas marca a entidade para exclusão
             _context.Tarefas.Remove(tarefa);
     
-            // O SaveChanges vai no banco e executa o DELETE de fato
             _context.SaveChanges();
         }
         
         public IEnumerable<Tarefa> ObterTodas()
         {
-            // O Entity Framework vai no banco, faz um SELECT * FROM Tarefas e transforma em uma lista
             return _context.Tarefas.ToList();
         }
     }
