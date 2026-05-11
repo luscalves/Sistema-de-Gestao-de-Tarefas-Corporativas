c7889eb51183093c359578561955f89284be774d
Lucas Alves <114171968+luscalves@users.noreply.github.com>
2026-05-11 09:43:57 -0300

    Add reason parameter to BloquearTarefaUseCase

--- Application/UseCases/BloquearTarefaUseCase.cs
+++ Application/UseCases/BloquearTarefaUseCase.cs
@@ -11,13 +11,11 @@ public class BloquearTarefaUseCase
         _context = context;
     }
 
-    // Agora recebemos o motivo por parâmetro
     public void Executar(int id, string motivo)
     {
         var tarefa = _context.Tarefas.Find(id);
         if (tarefa == null) throw new Exception("Tarefa não encontrada.");
 
-        // Usamos a inteligência da sua Entidade!
         tarefa.Bloquear(motivo); 
 
         _context.SaveChanges();
