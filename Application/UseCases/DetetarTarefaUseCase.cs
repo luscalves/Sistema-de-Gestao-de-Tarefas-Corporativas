3ed83c10f37b4d922c4e9837ebec0e33f5117340
Lucas Alves <114171968+luscalves@users.noreply.github.com>
2026-05-11 09:45:26 -0300

    Remove comments from Executar method

--- Application/UseCases/DetetarTarefaUseCase.cs
+++ Application/UseCases/DetetarTarefaUseCase.cs
@@ -13,14 +13,11 @@ public class DeletarTarefaUseCase
 
     public void Executar(int id)
     {
-        // 1. Procura a tarefa no banco
         var tarefa = _context.Tarefas.Find(id);
-        
-        // 2. Se não achar, lança um erro
+
         if (tarefa == null) 
             throw new Exception("Tarefa não encontrada para exclusão.");
 
-        // 3. Remove do Entity Framework e salva
         _context.Tarefas.Remove(tarefa);
         _context.SaveChanges();
     }
