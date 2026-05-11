22821523c2740e1c7db5199f6fbafc35a53e59d4
Lucas Alves <114171968+luscalves@users.noreply.github.com>
2026-05-11 09:44:24 -0300

    Refactor CriarUsuarioUseCase to include password hashing

--- Application/UseCases/CriarUsuarioUseCase.cs
+++ Application/UseCases/CriarUsuarioUseCase.cs
@@ -11,16 +11,13 @@ public class CriarUsuarioUseCase
         _repository = repository;
     }
 
-    // Adicionamos a 'senha' como parâmetro aqui
     public void Executar(string nome, string email, string senha, Usuario.TipoDepartamento departamento)
     {
         if (_repository.ExisteEmail(email))
             throw new Exception("Este e-mail já está cadastrado.");
 
-        // 1. Gerar o Hash da senha antes de criar o objeto
         string senhaHash = BCrypt.Net.BCrypt.HashPassword(senha);
 
-        // 2. Agora passamos os 4 argumentos exigidos pelo construtor
         var novoUsuario = new Usuario(nome, email, senhaHash, departamento);
 
         _repository.Adicionar(novoUsuario);
