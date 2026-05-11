1e1e27f453ecb7023417ca977ad5b0404444d41b
Lucas Alves <114171968+luscalves@users.noreply.github.com>
2026-05-11 09:47:25 -0300

    Remove comments from Executar method

--- Application/UseCases/RegistrarUsuarioUseCase.cs
+++ Application/UseCases/RegistrarUsuarioUseCase.cs
@@ -14,17 +14,13 @@ public class RegistrarUsuarioUseCase
 
     public void Executar(string nome, string email, string senhaAberta, Usuario.TipoDepartamento departamento)
     {
-        // 1. Verifica se o e-mail já existe
         if (_repository.ExisteEmail(email))
             throw new Exception("Este e-mail já está em uso por outro usuário.");
 
-        // 2. A MÁGICA DA SEGURANÇA: Criptografa a senha usando BCrypt
         string hashDaSenha = BCrypt.Net.BCrypt.HashPassword(senhaAberta);
 
-        // 3. Cria a entidade com a senha protegida
         var novoUsuario = new Usuario(nome, email, hashDaSenha, departamento);
 
-        // 4. Salva no banco de dados
         _repository.Adicionar(novoUsuario);
     }
 }
