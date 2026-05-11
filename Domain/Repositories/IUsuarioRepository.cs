7bf8cded2511e4d3d2036b9c7ef92f7896209f50
Lucas Alves <114171968+luscalves@users.noreply.github.com>
2026-05-11 09:50:29 -0300

    Update IUsuarioRepository.cs
    ae

--- Domain/Repositories/IUsuarioRepository.cs
+++ Domain/Repositories/IUsuarioRepository.cs
@@ -5,14 +5,7 @@ namespace SistemaDeGestaoDeTarefas;
 
 public interface IUsuarioRepository
 {
-    // O que já tínhamos
     Usuario ObterPorId(int id);
-    
-    // Novos poderes para atender o Front-end
-   
-    // IEnumerable<Usuario> ListarTodos();
-    
-    // Regra de negócio crucial: Evitar e-mails duplicados
     bool ExisteEmail(string email);
     Usuario ObterPorEmail(string email);
     void Adicionar(Usuario usuario);
